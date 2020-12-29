59b80821f6d88aa2628ea111554fe60400d03574
onotatu85 <onotatunonariagari@outlook.jp>
2020-12-29 12:42:59 +0900

    list6-3

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+const { url } = require("inspector");
+
 const port =3000,
 http = require("http"),
 httpStatus = require("http-status-codes"),
 fs = require("fs");
+
+const getViewUrl = (url) => {
+  return `views${url}.html`;
+
+};
 
 const routeMap = {
   "/": "views/index.html"
@@ -9,18 +16,22 @@ const routeMap = {
 
 http
 .createServer((req,res) => {
-  res.writeHead(httpStatus.OK,{
-    "Content-Type":"text/html"
-  });
-  
-  if (routeMap[req.url]) {
-    fs.readFile(routeMap[req.url],(error,data)  => {
-      res.write(data);
-      res.end();
-    });
-  } else {
-    res.end("<h1>Sorry,not found.</h1>");
-  }
+  let viewUrl = getViewUrl(req.url);
+
+  fs.readFile(viewUrl,(error,data) =>{
+    if(error){
+      res.writeHead(httpStatus.NOT_FOUND);
+      res.write("<h1>FILE NOT FOUND</h1>");
+    }else {
+      res.writeHead(httpStatus.OK,{
+        "Content-Type":"text/html"
+      });
+      res.write (data);
+    }
+    res.end();
+  });    
+    
+ 
 
 })
 
